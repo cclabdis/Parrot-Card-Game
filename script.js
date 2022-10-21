4ab1f8838faf6dc1ec9ebf7bf05dc7bc27fc6613
cclabdis <cclabdis@gmail.com>
2022-10-21 17:20:29 -0300

    reavaliação do projeto

--- script.js
+++ script.js
@@ -5,5 +5,25 @@ function jogo(){
   const jogadas = prompt("quantas cartas quer jogar?");
  
 }
-   
+  
 
+const cardList = [
+  "devilparrot.gif",
+  "unicornparrot.gif",
+  "negativeparrot.gif",
+  "brazilparrot.gif",
+  "drinkingparrot.gif",
+  "detectiveparrot.gif",
+  "fiestaparrot.gif",
+  "tripletsparrot.gif",
+  "metalparrot.gif",
+];
+
+
+// fazer cardList
+// fazer function de embaralhar
+// saber como fixa o faceUp
+
+function virar(elemento){
+  elemento.classList.toggle("faceUp")
+}
